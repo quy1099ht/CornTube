b5215ffa4d42565ac0cac6c5f7dcf6f3c2e2c862
NguyenXuanHa1688 <100813051+NguyenXuanHa1688@users.noreply.github.com>
2022-04-05 16:48:47 +0700

    similarity function for search controller

--- controlers/search.js
+++ controlers/search.js
@@ -13,5 +13,29 @@ exports.get = function(request,response){
     
 }
 exports.post = function(request,response){
-    return response.redirect('/')
+    Video.find(function(err,result){
+        var videos = result.filter((video) => similarity(video.name,request.body.search) > 0.20)
+        if(request.session.User)
+        {
+           return response.render('search',{videos : videos,logged : true, searchQuery : request.body.search})
+        }
+        response.render('search',{videos : videos,logged : false, searchQuery : request.body.search})
+    })
+    
 }
+
+
+
+function similarity(s1, s2) {
+    var longer = s1;
+    var shorter = s2;
+    if (s1.length < s2.length) {
+      longer = s2;
+      shorter = s1;
+    }
+    var longerLength = longer.length;
+    if (longerLength == 0) {
+      return 1.0;
+    }
+    return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
+  }
