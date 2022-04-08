92a1f3b27c753bb12855d2fcdff099233225870a
NguyenXuanHa1688 <100813051+NguyenXuanHa1688@users.noreply.github.com>
2022-04-08 19:58:28 +0700

    home function for controller

--- controlers/home.js
+++ controlers/home.js
@@ -1,13 +1,23 @@
-const { render } = require('express/lib/response')
 const mongo = require('mongo')
 const User = require('../models/user')
 const Video = require('../models/video')
 
 exports.get = function (request, response) {
 
-    return render("home",{
-        videos : [],
-        logged : false
+    Video.find(function (err, result) {
+        User.find(function (err, users) {
+            users.forEach(user => {
+                result.forEach(video => {
+                    if (video.creatorID == user.id) {
+                        video.creatorID = user.avatarURL
+                    }
+                })
+            })
+            if (request.session.User) {
+                return response.render('home', { videos: result, logged: true })
+            }
+            response.render('home', { videos: result, logged: false })
+        })
     })
 
 }
