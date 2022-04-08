f3963d4bb5605e50f4cba4dfdfbd5b5eacd22d69
NguyenXuanHa1688 <100813051+NguyenXuanHa1688@users.noreply.github.com>
2022-04-08 20:03:17 +0700

    register  function for controller

--- controlers/register.js
+++ controlers/register.js
@@ -1,14 +1,37 @@
 const User = require('../models/user')
 
 exports.get = function (request, response) {
+  response.render('signup')
 }
 function comparePassword(pass1, pass2) {
-  
+  if (pass1 == pass2) {
+    return true
+  }
+  return false
 }
 exports.post = async function (request, response) {
-  
+  fullname = request.body.fname + " " + request.body.lname
+  console.log(fullname)
+  if (!comparePassword(request.body.password, request.body.password2)) {
+    return res.status(500).send({ message: "Wrong" });
+  }
+  try {
+    const user = await User.create({
+      name: fullname,
+      email: request.body.email,
+      password: request.body.password,
+      dateOfBirth: getDate(),
+      avatarURL : "/images/avatars/default_avt.png",
+      country : ""
+    });
+  } catch (error) {
+    request.redirect("/signup")
+    res.status(500).send({ message: error.message });
+  }
+  response.redirect("/login")
 }
 
 function getDate() {
-  
+  var datetime = new Date();
+  return datetime.toISOString().slice(0, 10).replace('-', '/').replace('-', '/');
 }
