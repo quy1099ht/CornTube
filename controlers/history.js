ad2814cd3e2b1fd01fe6c0927f9f2b97c6351da5
NguyenXuanHa1688 <100813051+NguyenXuanHa1688@users.noreply.github.com>
2022-04-08 19:54:17 +0700

    history function for controller

--- controlers/history.js
+++ controlers/history.js
@@ -3,11 +3,49 @@ const History = require('../models/history')
 const User = require("../models/user")
 
 exports.get = async function (request, response) {
+    if (request.session.User) {
+        const videoss = []
+        await User.findOne({
+            email: request.session.User.user
+        }).then(async userData => {
+            await Video.find(async function (err, videos) {
+                await History.find( async function (err, histories) {
+                    if (err) {
+                        await console.log("Error2!!!");
+                    }
+                    for (let index = 0; index < histories.length; index++) {
+                        const element = await histories[index];
+                        if (element.userID == userData.id) {
+                            for (let index2 = 0; index2 < videos.length; index2++) {
+                                const element2 = await histories[index];
+                                if (element2.videoID == videos[index2].id) {
+                                    await videoss.push(videos[index2])
+                                }
+                            }
+                        }
+                    }
+                    return await response.render('history', { logged: true, videos: videoss.reverse() })
+                })
+                
+            })
 
+        })
+    }
     return response.redirect('/')
 }
 
 exports.deleteHistory = function(request,response){
-    
+    console.log("Something");
+    if(request.session.User){
+        User.findOne({
+            email: request.session.User.user
+        }).then(userData => {
+            History.deleteMany({
+                userID : userData.id
+            }).then(() => {
+                return response.redirect("/account")
+            })
+        })
+    }
     return response.redirect("/")
 }
